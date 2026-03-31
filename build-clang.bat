94d15a06d18e17c2f184f5e6a1d82f47cafd21b3
Richard Delorme <abulmo@users.noreply.github.com>
2026-03-31 10:18:11 +0100

    Update build-clang.bat for profiling and execution

--- build-clang.bat
+++ build-clang.bat
@@ -1,6 +1,9 @@
 del *.profraw
-clang -std=c23 -pedantic -W -Wall -O3 -flto -fuse-ld=lld -D_CRT_SECURE_NO_DEPRECATE -march=%1 -fprofile-instr-generate -fcoverage-mapping mperft.c -o .\mperft.exe
-.\mperft.exe -d 6 -h 64 -t 4 -b -q
+clang -std=c23 -pedantic -W -Wall -O3 -flto -fuse-ld=lld -D_CRT_SECURE_NO_DEPRECATE -march=%1 -fprofile-generate mperft.c -o .\mperft.exe
+.\mperft.exe -d 7 -h 64 -t 4 -n -q
+.\mperft.exe -d 7 -h 64 -t 4 -b -q
 llvm-profdata merge -sparse -output=mperft.profdata *.profraw
-clang -std=c23 -pedantic -W -Wall -O3 -flto -fomit-frame-pointer -fuse-ld=lld -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -march=%1 -fprofile-instr-use=mperft.profdata mperft.c -o .\mperft-%1.exe
+clang -std=c23 -pedantic -W -Wall -O3 -flto -fomit-frame-pointer -fuse-ld=lld -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE -march=%1 -fprofile-use=mperft.profdata mperft.c -o .\mperft-%1.exe
+del *.profraw *.profdata
+
 
